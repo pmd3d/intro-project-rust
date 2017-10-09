e27b462df36bfe250964ae239d7a9d170e979ccd
Jon <jjrr287@gmail.com>
2017-10-08 17:31:01 -0700

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+// todo: read in index.html
+// todo: rustfmt
+// todo: call start game from start link
 extern crate sdl2;
 extern crate iron;
 extern crate router;
+extern crate url;
+
+mod url_open;
 
 //use sdl2::pixels::Color;
 use sdl2::event::Event;
@@ -12,6 +18,11 @@ use iron::prelude::*;
 use iron::status;
 use router::Router;
 use std::path::Path;
+use url::Url;
+use url_open::UrlOpen;
+
+
+static APP_NAME: &'static str =  "Intro Project Rust";
 
 pub fn main() {
     let mut router = Router::new();
@@ -19,57 +30,69 @@ pub fn main() {
     router.get("/", handler, "index");
     router.get("/:query", handler, "query");
 
-    let _server = Iron::new(router).http("localhost:3000").unwrap();
+    // TODO: this need to change in case port is taken...
+    // more error handling too.
+    let _server = Iron::new(router).http("127.0.0.1:3000").unwrap();
 
     fn handler(req: &mut Request) -> IronResult<Response> {
         let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+
+        if (query == "start") {
+        }
+        else {
+           query = 
+        }
         Ok(Response::with((status::Ok, *query)))
     }
 
-    let sdl_context = sdl2::init().unwrap();
+    Url::parse("http://127.0.0.1:3000/").unwrap().open();
+
+    fn start_game() {
+        let sdl_context = sdl2::init().unwrap();
     
-    let mut timer = sdl_context.timer().unwrap();
+        let mut timer = sdl_context.timer().unwrap();
     
-    let video_subsystem = sdl_context.video().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-        .fullscreen()
-        .opengl()
-        .build()
-        .unwrap();
+       let window = video_subsystem.window(APP_NAME, 800, 600)
+           .fullscreen()
+           .opengl()
+           .build()
+           .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+       let mut canvas = window.into_canvas().build().unwrap();
 
-    let texture_creator = canvas.texture_creator();
+       let texture_creator = canvas.texture_creator();
 
-    let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("./assets/animate.bmp")).unwrap();
+       let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("./assets/animate.bmp")).unwrap();
 
-     let texture = texture_creator.create_texture_from_surface(&temp_surface).unwrap();
+       let texture = texture_creator.create_texture_from_surface(&temp_surface).unwrap();
 
-    let source_rect = Rect::new(0, 0, 128, 82);
-    let mut dest_rect = Rect::new(0, 0, 128, 82);
+       let source_rect = Rect::new(0, 0, 128, 82);
+       let mut dest_rect = Rect::new(0, 0, 128, 82);
 
-    let center = Point::new(320,240);
-    dest_rect.center_on(center);
+       let center = Point::new(320,240);
+       dest_rect.center_on(center);
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+       let mut event_pump = sdl_context.event_pump().unwrap();
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+       'running: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                   Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                       break 'running
+                   },
+                   _ => {}
+               }
+           }
+           ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 
-        let counter = timer.ticks();
+           let counter = timer.ticks();
 
-        dest_rect.set_x(((counter / 100) % 100) as i32);
-
-        canvas.copy_ex(&texture, Some(source_rect), Some(dest_rect), 10.0, None, true, false).unwrap();
-        canvas.present();
-    }
+           dest_rect.set_x(((counter / 100) % 100) as i32);
+  
+           canvas.copy_ex(&texture, Some(source_rect), Some(dest_rect), 10.0, None, true, false).unwrap();
+           canvas.present();
+       }
+   }
 }
